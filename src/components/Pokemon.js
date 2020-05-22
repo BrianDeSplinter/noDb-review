e382ada3239d5641c55f82c456cb1460950c35e9
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-22 00:25:00 -0600

    Finished Pokemon App

---
+++ src/components/Pokemon.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class Pokemon extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isEditing: false,
+      userInput: '',
+    }
+  }
+
+  toggleEdit() {
+    this.setState({
+      isEditing: !this.state.isEditing,
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value,
+    })
+  }
+
+  handleSaveName() {
+    const { data } = this.props
+    this.props.saveName(data.id, this.state.userInput)
+    this.toggleEdit()
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={this.props.data.image} alt={this.props.data.name} />
+        {!this.state.isEditing ? (
+          <p onDoubleClick={() => this.toggleEdit()}>{this.props.data.name}</p>
+        ) : (
+          <div>
+            <input onChange={(e) => this.handleChange(e)} />
+            <button onClick={() => this.handleSaveName()}>Save</button>
+            <button onClick={() => this.toggleEdit()}>Cancel</button>
+          </div>
+        )}
+        <button onClick={() => this.props.releasePokemon(this.props.data.id)}>
+          Release
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Pokemon
